1d5f2ebcaa342ce67026679811fcb1e5be35731d
sergeyt <sergej.tokarev@gmail.com>
2022-09-20 16:39:57 +0300

    DZ5 updated

--- 5/5_4.py
+++ 5/5_4.py
@@ -4,13 +4,26 @@
 
 import re
 
+#это сжатие
 def RLEconvert(file):
-
     f = open(file)
     strng = str(f.read())
-    outlist = ''.join([str(len(i))+i[0] for i in [m.group(0) for m in re.finditer(r"(.)\1*", strng)]])
+    outlist = ''.join([str(len(i)) + i[0] for i in [m.group(0) for m in re.finditer(r"(.)\1*", strng)]])
     f = open('5_4_output.txt', 'w')
     f.write(str(outlist))
 
+# это восстановление данных
+    templist = [str(len(i)) + i[0] for i in [m.group(0) for m in re.finditer(r"(.)\1*", strng)]]
+
+    def numstostr(strng):
+        numstr = [char for char in strng]
+        numstr1 = int(''.join(i for i in numstr if i.isdigit()))
+        return numstr1
+
+    listnums = [numstostr(i) for i in templist]
+    listletters = [i[-1] for i in templist if not i.isdigit()]
+    outlistdecoded = ''.join([listnums[i]*listletters[i] for i in range(len(listnums))])
+    f.write(str(' \n'))
+    f.write(str(outlistdecoded))
 
 RLEconvert('5_4_input.txt')
