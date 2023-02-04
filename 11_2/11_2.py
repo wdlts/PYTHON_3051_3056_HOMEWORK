a2ad4277411ee4c3c8b489392a4f3862185302fa
sergeyt <sergej.tokarev@gmail.com>
2023-02-04 21:02:03 +0300

    DZ6_CLIENT_SERVER

--- 11_2/11_2.py
+++ 11_2/11_2.py
@@ -1,14 +1,7 @@
-class Road:
+def sumFunc():
+    a = int(input())
+    b = int(input())
+    return int(a+b)
 
-    def __init__(self):
-        self.length = None
-        self.width = None
+sumFunc()
 
-    def result(self):
-        print(round(self.length*self.width*25*0.005))
-
-
-r = Road()
-r.length = 5000
-r.width = 20
-r.result()
